c8386dec5d536056f15175eeedc0d82acbfdcffe
Alan Russell <ajrussellaudio@gmail.com>
2024-02-25 04:13:31 +0000

    Make folder aliases 10x more awesome

--- .oh-my-zsh/custom/folder-aliases.zsh
+++ .oh-my-zsh/custom/folder-aliases.zsh
@@ -1,4 +1,10 @@
-alias work=/Users/alanrussell/Documents/working
-alias learn=/Users/alanrussell/Documents/learning
-alias play=/Users/alanrussell/Documents/playground
-alias oss=/Users/alanrussell/Documents/open-source
+create_or_cd () {
+  [ ! -d $1 ] && mkdir $1
+  cd $1
+}
+
+work () { create_or_cd ~/Documents/working }
+learn () { create_or_cd ~/Documents/learning }
+play () { create_or_cd ~/Documents/playground }
+oss () { create_or_cd ~/Documents/open-source }
+cfg () { create_or_cd ~/dotfiles } 
