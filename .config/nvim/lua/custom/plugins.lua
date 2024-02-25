8c114fc09a427494bc9a1479aa198ee69ce0480d
Alan Russell <ajrussellaudio@gmail.com>
2024-02-25 05:05:56 +0000

    Add missing nvim custom folder

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,64 @@
+local plugins = {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettierd",
+        "tailwindcss-language-server",
+        "typescript-language-server"
+      }
+    }
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "html",
+    },
+    config = function ()
+      require("nvim-ts-autotag").setup()
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function ()
+      local opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css"
+      }
+      return opts
+    end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },
+  {
+    
+  }
+}
+
+return plugins
