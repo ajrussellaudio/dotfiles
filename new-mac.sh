e28cda65feb3c9d3054c68a127d80a473d467739
Alan Russell <ajrussellaudio@gmail.com>
2024-02-25 15:06:57 +0000

    Fix script

--- new-mac.sh
+++ new-mac.sh
@@ -1,22 +1,45 @@
+# Mac shit
+echo "Setting up macOS preferences..."
+
+echo "- Requiring password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+SCREENSHOTS_FOLDER=~/Desktop/Screenshots
+echo "- Saving screenshots to" $SCREENSHOTS_FOLDER
+mkdir "$SCREENSHOTS_FOLDER"
+defaults write com.apple.screencapture location -string "$SCREENSHOTS_FOLDER"
+
+# Git shit
+
 echo "Creating an SSH key for you..."
 ssh-keygen -t rsa
-
 echo "Please add this public key to Github \n"
 echo "https://github.com/account/ssh \n"
 read -p "Press [Enter] key after this..."
 
+# Install shit
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "Installing Oh My Zsh..."
+  curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+fi
+
 if ! command -v brew &> /dev/null; then
-  echo "Homebrew not installed."
   echo "Installing Homebrew..."
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ./.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 echo "Installing all the things..."
 brew bundle
 
-# Git shit
-echo "Configuring git:"
-git config --global user.name "Alan Russell"
-git config --global user.email "ajrussellaudio@gmail.com"
+echo "Migrating dotfiles..."
+stow . --adopt
+
+if command -v tmux &> /dev/null; then
+  echo "Configuring tmux..."  
+  tmux source ~/.config/tmux/tmux.conf
+fi
+
