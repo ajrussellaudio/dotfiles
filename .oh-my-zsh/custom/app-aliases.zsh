f624981812bd41d0a4d5caba35e52aa88930699e
Alan Russell <ajrussellaudio@gmail.com>
2024-03-29 19:09:45 +0000

    Add tree sexiness

--- .oh-my-zsh/custom/app-aliases.zsh
+++ .oh-my-zsh/custom/app-aliases.zsh
@@ -2,7 +2,10 @@ alias bbd="rm -f ./Brewfile* && brew bundle dump --taps --brews --casks --mas"
 
 alias ls="eza"
 alias ll="eza -alh"
-alias tree="eza -TI node_modules"
+
+TREE_OPTIONS="--tree --group-directories-first --ignore-glob=\"node_modules|.git\""
+alias tree="eza $TREE_OPTIONS"
+alias treea="eza --all $TREE_OPTIONS"
 
 if command -v bat > /dev/null; then
   alias cat="bat"
