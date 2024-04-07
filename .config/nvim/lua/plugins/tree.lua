e4311d9061b416bda5c5b8d708183fa9a7ad710f
Alan Russell <ajrussellaudio@gmail.com>
2024-04-07 21:38:08 +0100

    Refresh Neotree on git status

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -7,21 +7,24 @@
 -- }
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	filesystem = {
+		use_libuv_file_watcher = true,
+	},
+	config = function()
+		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
-    vim.keymap.set("n", "<leader>gs", ":Neotree git_status reveal float<CR>")
-    vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal left<CR>")
-  end,
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
+		vim.keymap.set("n", "<leader>gs", ":Neotree git_status reveal float<CR>")
+		vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal left<CR>")
+	end,
 }
