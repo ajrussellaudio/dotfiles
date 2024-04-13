efa82f7726ddda9c7f9c9f2e09d537218b104954
Alan Russell <ajrussellaudio@gmail.com>
2024-04-13 23:08:07 +0100

    Remove commented code

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,30 +1,22 @@
--- return {
--- 	"nvim-tree/nvim-tree.lua",
--- 	config = function()
--- 		require("nvim-tree").setup()
--- 		vim.keymap.set("n", "<leader>n", ":NvimTreeFocus<CR>")
--- 	end,
--- }
-
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	filesystem = {
-		use_libuv_file_watcher = true,
-	},
-	config = function()
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  filesystem = {
+    use_libuv_file_watcher = true,
+  },
+  config = function()
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
-		vim.keymap.set("n", "<leader>gs", ":Neotree git_status reveal float<CR>")
-		vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal left<CR>")
-	end,
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
+    vim.keymap.set("n", "<leader>gs", ":Neotree git_status reveal float<CR>")
+    vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal left<CR>")
+  end,
 }
