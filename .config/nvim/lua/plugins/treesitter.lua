361a7f6fbf88918373515f20d9f73363eb1e2c95
Alan Russell <ajrussellaudio@gmail.com>
2024-04-14 00:42:09 +0100

    Add a bunch of plugins from Neovim-zen

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,38 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {
-        -- neovim
-        "lua",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				-- neovim
+				"lua",
+				"regex",
+				"markdown",
+				"markdown_inline",
 
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+				-- web dev
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			context_commentstring = {
+				config = {
+					javascript = {
+						__default = "// %s",
+						jsx_element = "{/* %s */}",
+						jsx_fragment = "{/* %s */}",
+						jsx_attribute = "// %s",
+						comment = "// %s",
+					},
+					typescript = { __default = "// %s", __multiline = "/* %s */" },
+				},
+			},
+		})
+	end,
 }
