798a4b197f732a96226b579dbbd39c6a5097b84a
Alan Russell <ajrussellaudio@gmail.com>
2024-05-19 15:30:06 +0100

    Add f alias for fzf

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,11 +8,11 @@ TREE_OPTIONS="--tree --group-directories-first --ignore-glob=\"node_modules|.git
 alias tree="eza $TREE_OPTIONS"
 alias treea="eza --all $TREE_OPTIONS"
 
-if command -v bat > /dev/null; then
-  alias cat="bat"
-elif command -v batcat > /dev/null; then
-  alias cat="batcat"
-fi
+alias cat="bat"
+
+export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"
+
+alias f="fzf"
 
 # Folder aliases
 create_or_cd () {
