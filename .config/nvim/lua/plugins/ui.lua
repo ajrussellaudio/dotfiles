7defe305243aa6634f0f86159274247445f9ec62
Alan Russell <ajrussellaudio@gmail.com>
2024-06-09 04:40:49 +0100

    Hide useless lualine sections

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -20,6 +20,22 @@ return {
 					section_separators = { left = "", right = "" },
 					component_separators = { left = "", right = "" },
 				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
 			})
 		end,
 	},
