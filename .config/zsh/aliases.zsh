198f1f629c70b9d10b4c71d173324031ce006011
Alan Russell <ajrussellaudio@gmail.com>
2024-08-08 00:14:10 +0100

    Pimp cat aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,17 +4,18 @@ alias k='clear'
 alias ls="eza"
 alias ll="eza -alh"
 
+alias grep="rg"
+
 TREE_OPTIONS="--tree --group-directories-first --ignore-glob=\"node_modules|.git\""
 alias tree="eza $TREE_OPTIONS"
 alias treea="eza --all $TREE_OPTIONS"
 
-alias cat="bat"
-
 export FZF_DEFAULT_OPTS="--preview 'bat --plain --color=always --tabs=2 {}'"
-
 alias f="fzf"
 
-alias grep="rg"
+alias cat="bat"
+alias catp="bat --plain"
+alias catf='bat $(fd --hidden --type f --exclude .git | fzf-tmux -p --reverse)'
 
 alias nv='nvim $(fd --hidden --type f --exclude .git | fzf-tmux -p --reverse)'
 
