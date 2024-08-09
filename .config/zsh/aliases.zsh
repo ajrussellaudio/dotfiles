f41dc1237836f075b544fcd62f9ce01f2bb60046
Alan Russell <ajrussellaudio@gmail.com>
2024-08-09 20:48:14 +0100

    Catppuccin for fzf

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,7 +10,14 @@ TREE_OPTIONS="--tree --group-directories-first --ignore-glob=\"node_modules|.git
 alias tree="eza $TREE_OPTIONS"
 alias treea="eza --all $TREE_OPTIONS"
 
-export FZF_DEFAULT_OPTS="--preview 'bat --plain --color=always --tabs=2 {}'"
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi \
+--preview 'bat --plain --color=always --tabs=2 {}'"
+
 alias f="fzf"
 
 alias cat="bat"
