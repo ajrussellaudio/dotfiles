145db5728708b25ec53940269e8119e30d79ed76
Alan Russell <ajrussellaudio@gmail.com>
2024-11-20 14:03:47 +0000

    Configure snacks dashboard

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,48 @@
+return {
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+        dashboard = {
+            enabled = true,
+            preset = {
+                keys = {
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    {
+                        icon = " ",
+                        key = "g",
+                        desc = "Find Text",
+                        action = ":lua Snacks.dashboard.pick('live_grep')",
+                    },
+                    {
+                        icon = " ",
+                        key = "c",
+                        desc = "Config",
+                        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+                    },
+                    { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                    {
+                        icon = "󰒲 ",
+                        key = "L",
+                        desc = "Lazy",
+                        action = ":Lazy",
+                        enabled = package.loaded.lazy ~= nil,
+                    },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
+            sections = {
+                { section = "header" },
+                { section = "keys",  gap = 1 },
+                {
+                    icon = " ",
+                    title = "Recent Files",
+                    section = "recent_files",
+                    indent = 2,
+                    padding = { 2, 2 },
+                },
+                { section = "startup" },
+            },
+        },
+    },
+}
