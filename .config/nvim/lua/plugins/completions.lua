804f540eeca17ab90226bbd309cd100e7cf4ac35
Alan Russell <ajrussellaudio@gmail.com>
2025-03-11 09:34:01 +0000

    Disable cmp in oil buffers

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,134 +1,149 @@
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-	Text = "󰊄",
-	Method = "",
-	Function = "󰊕",
-	Constructor = "",
-	Field = "",
-	Variable = "󰫧",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-	Copilot = "",
+    Text = "󰊄",
+    Method = "",
+    Function = "󰊕",
+    Constructor = "",
+    Field = "",
+    Variable = "󰫧",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
+    Copilot = "",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_snipmate").lazy_load()
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
+    {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip.loaders.from_snipmate").lazy_load()
+        end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						else
-							fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function()
-						if cmp.visible() then
-							cmp.select_prev_item()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "copilot", group_index = 2 },
-					-- Other Sources
-					{ name = "nvim_lsp", group_index = 2 },
-					{ name = "buffer", group_index = 2 },
-					{ name = "path", group_index = 2 },
-					{ name = "luasnip", group_index = 2 },
-				}),
-				formatting = {
-					fields = { "kind", "abbr", "menu" },
-					format = function(_, vim_item)
-						-- Kind icons
-						vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-						-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-						-- vim_item.menu = ({
-						-- 	nvim_lsp = "[LSP]",
-						-- 	luasnip = "[Snippet]",
-						-- 	buffer = "[Buffer]",
-						-- 	path = "[Path]",
-						-- 	copilot = "[Copilot]",
-						-- })[entry.source.name]
-						return vim_item
-					end,
-				},
-			})
+            cmp.setup({
+                enabled = function()
+                    local blacklist = {
+                        "oil",
+                    }
 
-			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
+                    local buffer = vim.bo.filetype
 
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-				matching = { disallow_symbol_nonprefix_matching = false },
-			})
+                    for _, f in ipairs(blacklist) do
+                        if buffer == f then
+                            return false
+                        end
+                    end
 
-			-- -- Set up lspconfig.
-			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			-- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-			-- require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
-			--     capabilities = capabilities,
-			-- })
-		end,
-	},
+                    return true
+                end,
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function()
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "copilot", group_index = 2 },
+                    -- Other Sources
+                    { name = "nvim_lsp", group_index = 2 },
+                    { name = "buffer", group_index = 2 },
+                    { name = "path", group_index = 2 },
+                    { name = "luasnip", group_index = 2 },
+                }),
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = function(_, vim_item)
+                        -- Kind icons
+                        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+                        -- vim_item.menu = ({
+                        -- 	nvim_lsp = "[LSP]",
+                        -- 	luasnip = "[Snippet]",
+                        -- 	buffer = "[Buffer]",
+                        -- 	path = "[Path]",
+                        -- 	copilot = "[Copilot]",
+                        -- })[entry.source.name]
+                        return vim_item
+                    end,
+                },
+            })
+
+            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+
+            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                        { name = "cmdline" },
+                    }),
+                matching = { disallow_symbol_nonprefix_matching = false },
+            })
+
+            -- -- Set up lspconfig.
+            -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+            -- require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
+            --     capabilities = capabilities,
+            -- })
+        end,
+    },
 }
