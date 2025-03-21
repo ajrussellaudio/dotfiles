e204f79b26966f21dfcc308fa6f1f6c923e8703c
Alan Russell <ajrussellaudio@gmail.com>
2025-03-21 08:39:47 +0000

    Configure blink with Luasnip

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,20 +1,33 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    -- !Important! Make sure you're using the latest release of LuaSnip
+    -- `main` does not work at the moment
+    version = "v2.*",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_snipmate").lazy_load()
+    end,
+  },
   opts = {
+    snippets = { preset = "luasnip" },
+    -- ensure you have the `snippets` source (enabled by default)
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
     keymap = {
       preset = "enter",
-      ["<Tab>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          end
-        end,
-        "select_next",
-        "fallback",
-      },
+      ["<Tab>"] = { "select_next", "fallback" },
       ["<S-Tab>"] = { "select_prev", "fallback" },
     },
     completion = {
+      trigger = {
+        show_in_snippet = false,
+      },
       list = {
         selection = {
           preselect = false,
