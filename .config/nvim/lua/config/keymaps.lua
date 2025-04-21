5580b6e60d2897fc6b79f7b84950fe2e438da35b
Alan Russell <ajrussellaudio@gmail.com>
2025-04-22 00:07:46 +0100

    Add duplicate and comment line keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- https://www.reddit.com/r/neovim/s/ijMNrMACqz
+vim.keymap.set("n", "ycc", "yygccp", { remap = true, desc = "Duplicate and comment line" })
