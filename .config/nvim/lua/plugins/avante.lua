449560637b1790ab9d4e75d58c8a797fd8eb3dca
Alan Russell <ajrussellaudio@gmail.com>
2025-04-25 01:33:27 +0100

    Add Avante

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "copilot",
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,13 +1,29 @@
 return {
   "saghen/blink.cmp",
   dependencies = {
+    "Kaiser-Yang/blink-cmp-avante",
     "rafamadriz/friendly-snippets",
+  },
+  -- not confident that this is the right place for this
+  compat = {
+    "avante_commands",
+    "avante_mentions",
+    "avante_files",
   },
   opts = {
     snippets = { preset = "default" },
     -- ensure you have the `snippets` source (enabled by default)
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "avante", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        avante = {
+          module = "blink-cmp-avante",
+          name = "Avante",
+          opts = {
+            -- options for blink-cmp-avante
+          },
+        },
+      },
     },
     keymap = {
       preset = "enter",
