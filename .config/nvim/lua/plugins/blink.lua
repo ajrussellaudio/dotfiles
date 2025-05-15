c7af8e78044824ceb2058e3093666ca06933aa62
Alan Russell <ajrussellaudio@gmail.com>
2025-05-15 18:38:49 +0100

    Revert "Avante (#10)"
    This reverts commit bece022b0a406dee03aa2fe6f50d620a4d9c37ce.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,29 +1,13 @@
 return {
   "saghen/blink.cmp",
   dependencies = {
-    "Kaiser-Yang/blink-cmp-avante",
     "rafamadriz/friendly-snippets",
-  },
-  -- not confident that this is the right place for this
-  compat = {
-    "avante_commands",
-    "avante_mentions",
-    "avante_files",
   },
   opts = {
     snippets = { preset = "default" },
     -- ensure you have the `snippets` source (enabled by default)
     sources = {
-      default = { "avante", "lsp", "path", "snippets", "buffer" },
-      providers = {
-        avante = {
-          module = "blink-cmp-avante",
-          name = "Avante",
-          opts = {
-            -- options for blink-cmp-avante
-          },
-        },
-      },
+      default = { "lsp", "path", "snippets", "buffer" },
     },
     keymap = {
       preset = "enter",
