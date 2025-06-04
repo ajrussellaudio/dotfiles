0eb3e259fad7ae84d4e7a146e49c33873b7deebf
Alan Russell <ajrussellaudio@gmail.com>
2025-06-04 10:38:02 +0100

    Ensure Mason LSPs installed

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      "css-lsp",
+      "dockerfile-language-server",
+      "emmet-language-server",
+      "html-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "prettier",
+      "shfmt",
+      "stylelint-lsp",
+      "stylua",
+      "taplo",
+      "vtsls",
+      "yaml-language-server",
+    },
+  },
+}
