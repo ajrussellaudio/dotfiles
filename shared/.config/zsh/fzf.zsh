66e1cdbabae6a73b5bb287230082731c3a7a9735
Alan Russell <ajrussellaudio@gmail.com>
2025-07-06 19:36:03 +0100

    Open fzf in a tmux popup

--- shared/.config/zsh/fzf.zsh
+++ shared/.config/zsh/fzf.zsh
@@ -3,6 +3,7 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
---multi"
+--multi \
+--tmux 50%"
 
 alias f="fzf --preview 'bat --plain --color=always --tabs=2 {}'"
