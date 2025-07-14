1d63a9a8ae59ee09bc386e7bf7c2fa6f63642f5d
Alan Russell <ajrussellaudio@gmail.com>
2025-07-14 15:46:55 +0100

    Add CodeCompanion and MCPHub

--- shared/.config/nvim/lua/config/keymaps.lua
+++ shared/.config/nvim/lua/config/keymaps.lua
@@ -13,3 +13,11 @@ vim.keymap.set(
   { desc = "Align to = char" }
 )
 vim.keymap.set({ "v" }, "<leader>+", ":s/ \\+= / = /g<CR>", { desc = "Remove = char alignment" })
+
+-- CodeCompanion
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
