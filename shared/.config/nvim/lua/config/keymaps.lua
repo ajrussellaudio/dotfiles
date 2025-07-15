2ddadd60b7e038474409a51d61ba343890f41135
Alan Russell <ajrussellaudio@gmail.com>
2025-07-15 09:37:06 +0100

    Edit keymaps

--- shared/.config/nvim/lua/config/keymaps.lua
+++ shared/.config/nvim/lua/config/keymaps.lua
@@ -15,8 +15,8 @@ vim.keymap.set(
 vim.keymap.set({ "v" }, "<leader>+", ":s/ \\+= / = /g<CR>", { desc = "Remove = char alignment" })
 
 -- CodeCompanion
-vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>ap", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
