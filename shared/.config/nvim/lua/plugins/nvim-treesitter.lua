6f4463987b67455d3ea20bd86c6cb9a545710189
Alan Russell <ajrussellaudio@gmail.com>
2025-07-18 17:44:29 +0100

    Add non-working nvim-treesitter config

---
+++ shared/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  -- main branch doesn't work with LazyVim :(
+  {
+    -- "nvim-treesitter/nvim-treesitter",
+    -- lazy = false,
+    -- branch = "main",
+    -- build = ":TSUpdate",
+  },
+}
