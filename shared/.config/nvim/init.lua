c1e3e5f8a00eac48206062df60601266ea1f4324
Alan Russell <ajrussellaudio@gmail.com>
2025-08-13 11:33:44 +0100

    Add tmux-navigation

--- shared/.config/nvim/init.lua
+++ shared/.config/nvim/init.lua
@@ -6,13 +6,13 @@ require 'lazy-init'
 
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup {
-  { import = 'plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+  { import = 'plugins' }, -- imports from lua/plugins/*.lua
 }
+
+-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+-- Or use telescope!
+-- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+-- you can continue same window with `<space>sr` which resumes last telescope search
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
