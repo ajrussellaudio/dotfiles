f5cf66d772a218e04ceab9a422a1cbb6d72659f5
Alan Russell <ajrussellaudio@gmail.com>
2025-08-13 19:29:37 +0100

    Move mini.files keymap out of setup

--- shared/.config/nvim/lua/plugins/mini.lua
+++ shared/.config/nvim/lua/plugins/mini.lua
@@ -36,14 +36,13 @@ return {
       require('mini.animate').setup()
 
       -- File browser
-      require('mini.files').setup {
-        vim.keymap.set('n', '-', function()
-          local buf_name = vim.api.nvim_buf_get_name(0)
-          local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
-          MiniFiles.open(path)
-          MiniFiles.reveal_cwd()
-        end, { desc = 'Open mini.files' }),
-      }
+      require('mini.files').setup {}
+      vim.keymap.set('n', '-', function()
+        local buf_name = vim.api.nvim_buf_get_name(0)
+        local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+        MiniFiles.open(path)
+        MiniFiles.reveal_cwd()
+      end, { desc = 'Open mini.files' })
     end,
   },
 }
