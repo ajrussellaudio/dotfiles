1ad9d821886881bbd84cc935e5fc9d54fe55a4c6
Alan Russell <ajrussellaudio@gmail.com>
2025-08-14 00:13:54 +0100

    Use dot for recent files in dashboard

--- shared/.config/nvim/lua/plugins/snacks.lua
+++ shared/.config/nvim/lua/plugins/snacks.lua
@@ -20,7 +20,7 @@ return {
           keys = {
             { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
             { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = ' ', key = '.', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = '󰁯 ', key = 's', desc = 'Restore Session', section = 'session' },
             { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
             { icon = '󱌢 ', key = 'M', desc = 'Mason', action = ':Mason' },
