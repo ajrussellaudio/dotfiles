11a3e75640df64410d83ae59a6418d8a20ce325c
Alan Russell <ajrussellaudio@gmail.com>
2025-08-14 00:39:04 +0100

    Tidy up snacks dashboard

--- shared/.config/nvim/lua/plugins/snacks.lua
+++ shared/.config/nvim/lua/plugins/snacks.lua
@@ -10,30 +10,25 @@ return {
         },
       },
     },
-    ---@type snacks.Config
     opts = {
       dashboard = {
-        -- your dashboard configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         preset = {
           keys = {
             { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
             { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = ' ', key = '.', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = '󰁯 ', key = 's', desc = 'Restore Session', section = 'session' },
-            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
-            { icon = '󱌢 ', key = 'M', desc = 'Mason', action = ':Mason' },
+            { icon = '󰒲 ', key = 'l', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = '󱌢 ', key = 'm', desc = 'Mason', action = ':Mason' },
             { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
           },
         },
         sections = {
-          -- { section = 'header' },
           {
             section = 'terminal',
             cmd = 'stormy',
             hl = 'header',
-            indent = 12,
+            indent = 10,
           },
           { section = 'keys', gap = 1, padding = 1 },
           { section = 'startup' },
