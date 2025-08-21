d2e424b28d91f61f1662fe8f32c221388eb2111d
Alan Russell <ajrussellaudio@gmail.com>
2025-08-21 17:29:08 +0100

    Add LspRestart shortcut

--- shared/.config/nvim/lua/config/keymaps.lua
+++ shared/.config/nvim/lua/config/keymaps.lua
@@ -56,3 +56,5 @@ map('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
 map('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
 map('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
 map('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
+
+map('n', '<C-w>r', '<cmd>LspRestart<CR>', { silent = false, desc = 'LSP Restart' })
