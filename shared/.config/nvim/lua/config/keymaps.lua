bfa7b3c82907655732c61219265abbfaedc8b108
Alan Russell <ajrussellaudio@gmail.com>
2025-08-22 10:55:19 +0100

    Notify on LSP restart

--- shared/.config/nvim/lua/config/keymaps.lua
+++ shared/.config/nvim/lua/config/keymaps.lua
@@ -57,7 +57,10 @@ map('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
 map('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
 map('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
 
-map('n', '<C-w>r', '<cmd>LspRestart<CR>', { silent = false, desc = 'LSP Restart' })
+map('n', '<C-w>r', function()
+  vim.cmd 'LspRestart'
+  vim.notify 'LSP Restarted'
+end, { silent = false, desc = 'LSP Restart' })
 
 map('n', 'K', function()
   vim.lsp.buf.hover { border = 'single' }
