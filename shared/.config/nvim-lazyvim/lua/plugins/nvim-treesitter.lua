4d113e1979dd890b250deb99b86d9ed7b8d5b6cb
Alan Russell <ajrussellaudio@gmail.com>
2025-08-25 20:35:25 +0100

    RIP LazyVim

--- shared/.config/nvim-lazyvim/lua/plugins/nvim-treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- main branch doesn't work with LazyVim :(
-  {
-    -- "nvim-treesitter/nvim-treesitter",
-    -- lazy = false,
-    -- branch = "main",
-    -- build = ":TSUpdate",
-  },
-}
