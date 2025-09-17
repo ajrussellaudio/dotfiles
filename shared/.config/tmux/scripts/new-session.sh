999e2dd0a990926d4d22d2c142d1a0674fc9ecfe
Alan Russell <ajrussellaudio@gmail.com>
2025-09-18 00:19:59 +0100

    [tmux] Better new session

---
+++ shared/.config/tmux/scripts/new-session.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+tput setaf 2
+echo -n "   "
+tput sgr0
+
+# bash
+# read -p "New session name: " NAME
+
+# zsh
+read "NAME?New session name: "
+
+tmux new-session -d -s $NAME
+tmux switch-client -t $NAME
+
