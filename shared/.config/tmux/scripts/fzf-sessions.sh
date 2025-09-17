b538532a446ad256defc6a072791833d9d2af3c1
Alan Russell <ajrussellaudio@gmail.com>
2025-09-18 00:00:20 +0100

    [tmux] Better session switcher

---
+++ shared/.config/tmux/scripts/fzf-sessions.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+tmux list-sessions -F "#S" \
+    | grep -v $(tmux display-message -p "#S") \
+    | fzf \
+        --tmux 80% \
+        --reverse \
+        --preview="tmux capture-pane -pe -t {} | bat -p" \
+        --preview-window=right,80% \
+        --preview-border=line \
+        --ghost="Session name" \
+        --header="Available Sessions:" \
+        --border \
+        --border=rounded \
+        --border-label=" $1 " \
+        --color=label:italic:black \
