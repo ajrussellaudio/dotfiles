a1eca5a3561f76c8c1ede5a4b5c7c421f8385118
Alan Russell <ajrussellaudio@gmail.com>
2025-10-23 14:54:18 +0100

    [tmux] Include shells in dotfiles windows

--- shared/.config/tmux/layouts/dotfiles.session.sh
+++ shared/.config/tmux/layouts/dotfiles.session.sh
@@ -14,11 +14,15 @@ if initialize_session "dotfiles"; then
 
     window_root "$session_root/shared/.config/nvim"
     new_window "nvim"
-    run_cmd "nvim"
+    split_h 20
+    run_cmd "nvim" 1
+    select_pane 1
 
     window_root "$session_root/shared/.config/tmux"
     new_window "tmux"
-    run_cmd "nvim"
+    split_h 20
+    run_cmd "nvim" 1
+    select_pane 1
 
     select_window 1
 
