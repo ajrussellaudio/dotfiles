a7d2bdc729b9e985a13d5939aaacde38bec2e3cf
Alan Russell <ajrussellaudio@gmail.com>
2025-10-23 10:58:20 +0100

    [nvim] Format CSS with Prettier

--- shared/.config/nvim/lua/plugins/conform.lua
+++ shared/.config/nvim/lua/plugins/conform.lua
@@ -36,7 +36,7 @@ return {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         html = { 'prettierd', 'prettier', stop_after_first = true },
-        -- TODO: CSS
+        css = { 'prettierd', 'prettier', stop_after_first = true },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
