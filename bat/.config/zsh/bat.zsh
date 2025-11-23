b24bd9cfad593e5d128c9ba77c8c30621b0aa5fc
Alan Russell <ajrussellaudio@gmail.com>
2025-11-23 16:48:37 +0000

    Move bat zsh config to bat pkg

---
+++ bat/.config/zsh/bat.zsh
@@ -0,0 +1,2 @@
+alias cat="bat"
+alias catp="bat --plain"
--- zsh/.config/zsh/cat.zsh
+++
@@ -1,3 +0,0 @@
-alias cat="bat"
-alias catp="bat --plain"
-alias catf="fd --hidden --type f --exclude .git | fzf --reverse --bind 'enter:become(bat {})'"
