a5d4c39e5c9ac5a2c582743c917916f85e53611d
Alan Russell <ajrussellaudio@gmail.com>
2025-11-25 08:46:42 +0000

    Stow work stuff

--- install_work.sh
+++ install_work.sh
@@ -2,8 +2,8 @@
 
 source "$HOME/dotfiles/_install/utils.sh"
 
+_safely_stow add work
+
 do_install "bruno"
 do_install "elgato-control-center"
 do_install "elgato-stream-deck"
-# aws-cli
-# aws-vault
