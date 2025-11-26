55e11d30e49beed588301f1462d77cdd64a437a7
Alan Russell <ajrussellaudio@gmail.com>
2025-11-26 22:46:48 +0000

    [nvim] Full Markdown experience

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -16,9 +16,6 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
@@ -30,17 +27,16 @@ return {
         end
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        html = { 'prettierd', 'prettier', stop_after_first = true },
-        css = { 'prettierd', 'prettier', stop_after_first = true },
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        bash = { 'shfmt' },
+        css = { 'prettierd' },
         go = { 'goimports', 'gofumpt' },
+        html = { 'prettierd' },
+        javascript = { 'prettierd' },
+        lua = { 'stylua' },
+        markdown = { 'prettierd' },
+        typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        yaml = { 'prettierd' },
       },
     },
   },
