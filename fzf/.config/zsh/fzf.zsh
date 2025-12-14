c207ac021ce7975a56a6266f2278d9ca48c91ecd
Alan Russell <ajrussellaudio@gmail.com>
2025-12-14 19:05:19 +0000

    [fzf] Merge laptop changes

--- fzf/.config/zsh/fzf.zsh
+++ fzf/.config/zsh/fzf.zsh
@@ -2,13 +2,13 @@
 eval "$(fzf --zsh)"
 
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
---color=selected-bg:#45475a \
---layout=reverse \
---multi \
---tmux 50%"
+  --reverse \
+  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+  --color=selected-bg:#45475a \
+  --multi \
+  --tmux 80%"
 
 alias f="fzf --preview 'bat --plain --color=always --tabs=2 {}'"
 
@@ -24,7 +24,10 @@ _fzf_comprun() {
         then bat --plain --color=always --tabs=2 {}; \
         else echo "Preview not available."; \
         fi' ;;
-    nvim) fzf "$@" --walker file,follow,hidden --preview 'if [ -d {} ]; \
+    nvim) fzf "$@" \
+      --walker file,follow,hidden
+      --prompt 'nvim > ' \
+      --preview 'if [ -d {} ]; \
         then eza --tree --all --icons --color=always {}; \
         elif [ -f {} ]; \
         then bat --plain --color=always --tabs=2 {}; \
