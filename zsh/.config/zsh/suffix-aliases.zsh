3a9864052e6341ae9cbe3a8bf4f037ff4ccb70e9
Alan Russell <ajrussellaudio@gmail.com>
2026-01-03 12:29:52 +0000

    Add suffix aliases

---
+++ zsh/.config/zsh/suffix-aliases.zsh
@@ -0,0 +1,20 @@
+function this_or_that() {
+  local this=$1
+  shift
+  local that=$1
+  shift
+  (( $+commands[$this] )) && $this $@ || $that $@
+}
+
+alias -s json='this_or_that jless bat'
+alias -s md='this_or_that glow bat'
+alias -s txt=bat
+alias -s log=bat
+alias -s js='$EDITOR'
+alias -s jsx='$EDITOR'
+alias -s cjs='$EDITOR'
+alias -s mjs='$EDITOR'
+alias -s ts='$EDITOR'
+alias -s tsx='$EDITOR'
+alias -s mts='$EDITOR'
+alias -s html=open  # macOS: open in default browser
