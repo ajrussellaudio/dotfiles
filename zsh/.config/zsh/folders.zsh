6495161034865eb240e30bfff2692fe6f4f1b457
Alan Russell <ajrussellaudio@gmail.com>
2026-01-03 16:21:37 +0000

    Use hash for common folders

--- zsh/.config/zsh/folders.zsh
+++ zsh/.config/zsh/folders.zsh
@@ -1,10 +1,16 @@
+hash -d work="$HOME/Documents/working"
+hash -d learn="$HOME/Documents/learning"
+hash -d play="$HOME/Documents/playground"
+hash -d oss="$HOME/Documents/open-source"
+hash -d dots="$HOME/dotfiles"
+
 create_or_cd () {
   [ ! -d $1 ] && mkdir $1
   cd $1
 }
 
-work () { create_or_cd ~/Documents/working }
-learn () { create_or_cd ~/Documents/learning }
-play () { create_or_cd ~/Documents/playground }
-oss () { create_or_cd ~/Documents/open-source }
-cfg () { create_or_cd ~/dotfiles } 
+work () { create_or_cd ~work }
+learn () { create_or_cd ~learn }
+play () { create_or_cd ~play }
+oss () { create_or_cd ~oss }
+cfg () { create_or_cd ~dots } 
